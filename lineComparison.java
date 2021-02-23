912e3544effa1f046869425d067a29f80f5bdd75
kunalkale98 <kunal,k2898@gmail.com>
2021-02-24 00:46:16 +0530

    Added: Length of Line

--- lineComparison.java
+++ lineComparison.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+import java.lang.Math;
 class lineComparison{
-	public static void main(String[] args){
-		System.out.println("Welcome to Line Comparison Computation");
+
+	public float lengthofline(){
+		System.out.println("Enter coordinates of point to get the length");
+      Scanner sc = new Scanner(System.in);
+      System.out.print("x1: ");
+      int x1 = sc.nextInt();
+      System.out.print("y1: ");
+      int y1 = sc.nextInt();
+      System.out.print("x2: ");
+      int x2 = sc.nextInt();
+      System.out.print("y2: ");
+      int y2 = sc.nextInt();
+		float length = (float)Math.sqrt( Math.pow( (x2-x1),2 ) + Math.pow( (y2-y1),2 ) );
+		return length;
 	}
+
+	 public static void main(String[] args){
+      lineComparison ln = new lineComparison();
+      float length1 = ln.lengthofline();
+		System.out.println("lenght of the line is "+length1);
+   }
+
 }
